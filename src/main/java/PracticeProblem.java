fc959e4524755449fbd863c227cd4dd4010a206f
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-02-27 15:23:14 +0000

    WIP2

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -14,20 +14,44 @@ public static int calculate(int num, int num2, char c){
 			return num/num2;
 		case '%':
 			return num%num2;
-		case '^':
-			return num^num2;
+		default:
+			return (int)Math.pow((double)num, (double)num2);
 	}
+}
 
+final static int MAXIMUM = 10;
 public static boolean totalWordsCheacker(String words){
-	final int MAXIMUM = 10;
+	
 	words=words.trim();
 	String[] strArr = words.split(" ");
-	if(strArr.length>){
+	if(strArr.length > MAXIMUM){
 		return false;
 	}
 	return true;
 }
 	
+public static String minString(String word, String word2, String word3){
+	word = word.toLowerCase();
+	word2 = word2.toLowerCase();
+	word3 = word3.toLowerCase();
+	if (word.compareTo(word2) < 0){
+		if (word.compareTo(word3) < 0){
+return word;
+		}
+		else{
 
+		 return word2;
+		}
+	}
+	else if (word2.compareTo(word3) < 0){
+return word2;
+		}
+		else{
+
+		 return word3;
+		}
+
+	}
 }
-}
+
+
