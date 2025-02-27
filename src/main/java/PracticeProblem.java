e8eed7d0f2d0b706e64d73194ae9a859788b2d2b
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-02-27 15:02:34 +0000

    WIP

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,5 +2,32 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+public static int calculate(int num, int num2, char c){
+	switch (c){
+		case '+':
+			return num+num2;
+		case '-':
+			return num-num2;
+		case '*':
+			return num*num2;
+		case '/':
+			return num/num2;
+		case '%':
+			return num%num2;
+		case '^':
+			return num^num2;
+	}
 
+public static boolean totalWordsCheacker(String words){
+	final int MAXIMUM = 10;
+	words=words.trim();
+	String[] strArr = words.split(" ");
+	if(strArr.length>){
+		return false;
+	}
+	return true;
+}
+	
+
+}
 }
